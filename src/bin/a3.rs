d287505f97ff6b30c2bdd8e6db28929b2ff0bd51
Jörn Horstmann <git@jhorstmann.net>
2021-12-03 11:00:09 +0100

    Day 3 part 2

--- src/bin/a3.rs
+++ src/bin/a3.rs
@@ -1,50 +1,68 @@
 use adventofcode2021::prelude::*;
 
-
 pub fn main() -> Result<()> {
     let data = include_str!("../../data/a3_input.txt");
 
     let numbers = data.lines().map(|l| Ok(u64::from_str_radix(l, 2)?)).collect::<Result<Vec<u64>>>()?;
 
-
     let mut histogram = vec![0_usize; 64];
 
     for n in numbers.iter() {
         for b in 0..64 {
-            if n & (1<<b) != 0 {
+            if n & (1 << b) != 0 {
                 histogram[b] += 1;
             }
         }
     }
 
-    let digits = data.lines().map(|l| l.len()).max().unwrap();
+    let digits = histogram.len() - histogram.iter().rev().take_while(|hist| **hist == 0).count();
 
-    dbg!(digits);
-
-    let gamma = histogram.iter().take(digits).enumerate().map(| (i, hist)| {
-        if *hist >= numbers.len()/2 {
+    let gamma = histogram.iter().take(digits).enumerate().map(|(i, hist)| {
+        if *hist >= numbers.len() / 2 {
             1 << i
         } else {
             0
         }
     }).sum::<u64>();
 
-    let epsilon = histogram.iter().take(digits).enumerate().map(| (i, hist)| {
-        if *hist < numbers.len()/2 {
-            1 << i
-        } else {
-            0
-        }
-    }).sum::<u64>();
-
-    let epsilon = ((!gamma) & ((1 << digits)-1));
-
+    let epsilon = (!gamma) & ((1 << digits) - 1);
 
     println!("{:012b} * {:012b} = {}", gamma, epsilon, gamma * epsilon);
     println!("{} * {} = {}", gamma, epsilon, gamma * epsilon);
 
+    let mut ox_rating = numbers.clone();
+    for bit in (0..digits).rev() {
+        let len = ox_rating.len();
+        let count_ones = ox_rating.iter().filter(|n| **n & (1 << bit) != 0).count();
+
+        let search_bit = count_ones * 2 >= len;
+
+        ox_rating.retain(|n| (n & (1 << bit) != 0) == search_bit);
+
+        if ox_rating.len() <= 1 {
+            break;
+        }
+    }
+
+    let ox_rating = ox_rating[0];
+
+    let mut co2_rating = numbers.clone();
+    for bit in (0..digits).rev() {
+        let len = co2_rating.len();
+        let count_ones = co2_rating.iter().filter(|n| **n & (1 << bit) != 0).count();
+
+        let search_bit = !(count_ones * 2 >= len);
+
+        co2_rating.retain(|n| (n & (1 << bit) != 0) == search_bit);
+
+        if co2_rating.len() <= 1 {
+            break;
+        }
+    }
+
+    let co2_rating = co2_rating[0];
+
+    println!("{} * {} = {}", ox_rating, co2_rating, ox_rating * co2_rating);
 
     Ok(())
-
-
 }
