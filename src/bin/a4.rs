0ae07b0a99e8c708ccaeb318de9b0f2afb9417eb
Jörn Horstmann <git@jhorstmann.net>
2021-12-04 10:44:43 +0100

    Error handling and formatting

--- src/bin/a4.rs
+++ src/bin/a4.rs
@@ -1,22 +1,32 @@
 use adventofcode2021::prelude::*;
 
-fn read_board<'a>(lines: impl Iterator<Item=&'a str>) -> [[i32; 5]; 5] {
+fn read_board<'a>(lines: impl Iterator<Item = &'a str>) -> Result<[[i32; 5]; 5]> {
     let mut result = [[0; 5]; 5];
-    result.iter_mut().zip(lines).for_each(|(row, line)| {
-        row.iter_mut().zip(line.split_ascii_whitespace()).for_each(|(r, n)| *r = i32::from_str(n).unwrap());
-    });
-    result
+    result.iter_mut().zip(lines).try_for_each(|(row, line)| {
+        row.iter_mut()
+            .zip(line.split_ascii_whitespace())
+            .try_for_each(|(r, n)| -> Result<()> {
+                *r = i32::from_str(n)?;
+                Ok(())
+            })
+    })?;
+    Ok(result)
 }
 
 pub fn main() -> Result<()> {
     let data = include_str!("../../data/a4_input.txt");
     let mut lines = data.lines().peekable();
-    let numbers = lines.next().ok_or(Error::EmptyIterator)?.split(",").map(|n| Ok(i32::from_str(n)?)).collect::<Result<Vec<i32>>>()?;
+    let numbers = lines
+        .next()
+        .ok_or(Error::EmptyIterator)?
+        .split(",")
+        .map(|n| Ok(i32::from_str(n)?))
+        .collect::<Result<Vec<i32>>>()?;
     let _empty_line = lines.next().ok_or(Error::EmptyIterator)?;
 
     let mut boards = vec![];
     while lines.peek().is_some() {
-        boards.push(read_board(&mut lines));
+        boards.push(read_board(&mut lines)?);
         if let Some(line) = lines.peek() {
             if line.is_empty() {
                 lines.next();
@@ -43,9 +53,11 @@ pub fn main() -> Result<()> {
                 let horz_win = (0..5).any(|row| marker[row].iter().all(|col| *col));
                 let vert_win = (0..5).any(|col| marker.iter().all(|m| m[col as usize]));
                 if horz_win || vert_win {
-                    let sum_unmarked: i32 = board.iter().zip(marker.iter()).flat_map(|(row, m)| {
-                        row.iter().zip(m.iter()).filter(|(_, m)| !**m).map(|(x, _)| *x)
-                    }).sum();
+                    let sum_unmarked: i32 = board
+                        .iter()
+                        .zip(marker.iter())
+                        .flat_map(|(row, m)| row.iter().zip(m.iter()).filter(|(_, m)| !**m).map(|(x, _)| *x))
+                        .sum();
                     let score = n * sum_unmarked;
 
                     // answer for part1 is first printed score, part2 is the last printed score
