7dbf3ab42943b6fd4334b3fcd52fbfc2460d29c5
Jörn Horstmann <git@jhorstmann.net>
2021-12-06 09:58:58 +0100

    Day 6 part 2

--- src/bin/a6.rs
+++ src/bin/a6.rs
@@ -1,15 +1,13 @@
 use adventofcode2021::prelude::*;
 
-pub fn main() -> Result<()> {
-    let data = include_str!("../../data/a6_example.txt");
-
-    let mut ages = data.split(",").map(|n| Ok(u32::from_str(n)?)).collect::<Result<Vec<u32>>>()?;
+fn part1(ages: &[u32], days: i32) -> usize {
+    let mut ages = ages.to_vec();
     let mut new_ages = Vec::with_capacity(4096);
 
-    for _day in 0..80 {
+    for _day in 0..days {
         new_ages.clear();
         for age in ages.iter() {
-            if let Some( age) = age.checked_sub(1) {
+            if let Some(age) = age.checked_sub(1) {
                 new_ages.push(age);
             } else {
                 new_ages.push(6);
@@ -20,7 +18,42 @@ pub fn main() -> Result<()> {
         std::mem::swap(&mut ages, &mut new_ages)
     }
 
-    println!("{}", ages.len());
+    ages.len()
+}
+
+fn part2(ages: &[u32], days: usize) -> usize {
+    let mut histogram = [0_usize; 9];
+    for age in ages.iter() {
+        histogram[*age as usize] += 1;
+    }
+
+    for _day in 0..days {
+        let mut count = histogram[0];
+        for i in 1..histogram.len() {
+            histogram[i - 1] = histogram[i]
+        }
+        histogram[6] += count;
+        histogram[8] = count;
+    }
+
+    histogram.iter().sum()
+}
+
+pub fn main() -> Result<()> {
+    let data = include_str!("../../data/a6_input.txt");
+
+    let ages = data
+        .split(",")
+        .map(|n| Ok(u32::from_str(n)?))
+        .collect::<Result<Vec<u32>>>()?;
+
+    println!("After  18 Days: {}", part1(&ages, 18));
+    println!("After  80 Days: {}", part1(&ages, 80));
+
+    println!("Part 2 optimized version");
+    for days in [18, 80, 256] {
+        println!("After {} Days: {}", days, part2(&ages, days));
+    }
 
     Ok(())
 }
