aaff1cd3b393cbaceae056ff552a82e7ba0a73f3
Jörn Horstmann <git@jhorstmann.net>
2021-12-08 13:23:09 +0100

    Day 8 formatting

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -39,26 +39,56 @@ fn part1(lines: &[Input]) -> usize {
 }
 
 fn part2_line(input: &Input) -> Result<u32> {
-    let one = *input.patterns.iter().find(|m| m.count_ones() == 2).ok_or(Error::General("one".into()))?;
-    let seven = *input.patterns.iter().find(|m| m.count_ones() == 3).ok_or(Error::General("seven".into()))?;
-    let eight = *input.patterns.iter().find(|m| m.count_ones() == 7).ok_or(Error::General("eight".into()))?;
-    let four = *input.patterns.iter().find(|m| m.count_ones() == 4).ok_or(Error::General("four".into()))?;
-    let zero = *input.patterns.iter().find(|m| m.count_ones() == 6 && (**m & !seven).count_ones() == 3 && (**m & !four).count_ones()==3).ok_or(Error::General("zero".into()))?;
-    let three = *input.patterns.iter().find(|m| m.count_ones() == 5 && (**m & !seven).count_ones() == 2).ok_or(Error::General("three".into()))?;
-    let nine = *input.patterns.iter().find(|m| m.count_ones() == 6 && (**m & seven).count_ones() == 3 && (**m & !four).count_ones()==2).ok_or(Error::General("nine".into()))?;
-    let six = *input.patterns.iter().find(|m| m.count_ones() == 6 && (**m & one).count_ones() == 1).ok_or(Error::General("six".into()))?;
-    let five = *input.patterns.iter().find(|m| m.count_ones() == 5 && (**m & one).count_ones() == 1 && (**m & four).count_ones() == 3).ok_or(Error::General("five".into()))?;
-    let two = *input.patterns.iter().find(|m| m.count_ones() == 5 && (**m & one).count_ones() == 1 && (**m & four).count_ones() == 2).ok_or(Error::General("two".into()))?;
+    let patterns = input.patterns;
+    let one = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 2)
+        .ok_or(Error::General("one".into()))?;
+    let seven = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 3)
+        .ok_or(Error::General("seven".into()))?;
+    let eight = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 7)
+        .ok_or(Error::General("eight".into()))?;
+    let four = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 4)
+        .ok_or(Error::General("four".into()))?;
+    let zero = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 6 && (**m & !seven).count_ones() == 3 && (**m & !four).count_ones() == 3)
+        .ok_or(Error::General("zero".into()))?;
+    let three = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 5 && (**m & !seven).count_ones() == 2)
+        .ok_or(Error::General("three".into()))?;
+    let nine = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 6 && (**m & seven).count_ones() == 3 && (**m & !four).count_ones() == 2)
+        .ok_or(Error::General("nine".into()))?;
+    let six = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 6 && (**m & one).count_ones() == 1)
+        .ok_or(Error::General("six".into()))?;
+    let five = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 5 && (**m & one).count_ones() == 1 && (**m & four).count_ones() == 3)
+        .ok_or(Error::General("five".into()))?;
+    let two = *patterns
+        .iter()
+        .find(|m| m.count_ones() == 5 && (**m & one).count_ones() == 1 && (**m & four).count_ones() == 2)
+        .ok_or(Error::General("two".into()))?;
 
     let numbers = [zero, one, two, three, four, five, six, seven, eight, nine];
 
     let result = input.output_values.iter().try_fold(0_u32, |a, m| -> Result<u32> {
         let digit_pos = numbers.iter().position(|n| *n == *m).ok_or(Error::EmptyIterator)?;
-        Ok(a*10 + digit_pos as u32)
+        Ok(a * 10 + digit_pos as u32)
     })?;
 
     Ok(result)
-
 }
 
 pub fn main() -> Result<()> {
@@ -76,7 +106,7 @@ pub fn main() -> Result<()> {
 
     println!("Part1: {}", part1(&lines));
 
-    let part2 = lines.iter().try_fold(0_u32,|sum, l| -> Result<u32>{
+    let part2 = lines.iter().try_fold(0_u32, |sum, l| -> Result<u32> {
         let num = part2_line(l)?;
         Ok(sum + num)
     })?;
