d2a4acbbd7cfe5f0580d95d763a092f4959dc471
Jörn Horstmann <git@jhorstmann.net>
2021-12-12 10:17:22 +0100

    Day 12 part 2

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -1,26 +1,43 @@
 use std::collections::HashMap;
 use adventofcode2021::prelude::*;
 
-struct Edge {
-    from_id: usize,
-    from_small: bool,
-    to_id: usize,
-    to_small: bool,
-}
-
-fn count_paths(edges: &[(usize, usize)], dict: &HashMap<usize, &str>, small_caves: u64, current: usize, end: usize, visited: u64, len: usize, found: &mut usize) {
-    eprintln!("Current {} ({})", dict.get(&current).unwrap_or(&""), *found);
-    // eprintln!("{:016b} ({})", visited, len);
+fn count_paths(edges: &[(usize, usize)], small_caves: u64, current: usize, end: usize, visited: u64, found: &mut usize) {
     if current == end {
         *found += 1
     } else {
         let visited = visited | (1 << current);
         for (from, to) in edges.iter().copied() {
             if from == current {
-                let is_small = small_caves & (1<<to) != 0;
-                let is_visited = visited & (1<<to) != 0;
+                let is_small = small_caves & (1 << to) != 0;
+                let is_visited = visited & (1 << to) != 0;
                 if !is_small || !is_visited {
-                    count_paths(edges, dict, small_caves, to, end, visited, len+1, found);
+                    count_paths(edges, small_caves, to, end, visited, found);
+                }
+            }
+        }
+    }
+}
+
+fn count_paths_part2(edges: &[(usize, usize)], small_caves: u64, current: usize, start: usize, end: usize, visited: u64, visited_small_twice: u64, found: &mut usize) {
+    if current == end {
+        *found += 1
+    } else {
+        let (visited, visited_small_twice) = if visited & (1<<current) != 0 {
+            if small_caves & (1<<current) != 0 {
+                (visited, visited_small_twice | (1 << current))
+            } else {
+                (visited, visited_small_twice)
+            }
+        } else {
+            (visited | (1 << current), visited_small_twice)
+        };
+        for (from, to) in edges.iter().copied() {
+            if from == current {
+                let is_small = small_caves & (1 << to) != 0;
+                let is_visited = visited & (1 << to) != 0;
+                let is_visited_small_twice = visited_small_twice & (1 << to) != 0 || to == start;
+                if !is_visited || !is_small || (visited_small_twice == 0 && !is_visited_small_twice) {
+                    count_paths_part2(edges, small_caves, to, start, end, visited, visited_small_twice, found);
                 }
             }
         }
@@ -29,7 +46,7 @@ fn count_paths(edges: &[(usize, usize)], dict: &HashMap<usize, &str>, small_cave
 
 pub fn main() -> Result<()> {
     let mut dict = HashMap::new();
-    let mut edges = include_str!("../../data/a12_example3.txt").lines().map(|line| {
+    let edges = include_str!("../../data/a12_input.txt").lines().map(|line| {
         let mut split = line.split("-");
         let from = split.next().ok_or(Error::EmptyIterator)?;
         let next_id = dict.len();
@@ -41,32 +58,35 @@ pub fn main() -> Result<()> {
         Ok((from_id, to_id))
     }).collect::<Result<Vec<(usize, usize)>>>()?;
 
-    let mut reverse_edges = edges.iter().map(|(from, to)| (*to, *from)).collect::<Vec<_>>();
-    edges.extend(&reverse_edges);
+    let edges = edges.iter()
+        .flat_map(|(from, to)| [(*from, *to), (*to, *from)].into_iter())
+        .collect::<Vec<_>>();
 
     if dict.len() > 64 {
         return Err(Error::General("too many edges".into()));
     }
 
-    let dict_values = dict.iter().map(|(key, value)| (*value, *key)).collect::<HashMap<_, _>>();
+    let _dict_values = dict.iter().map(|(key, value)| (*value, *key)).collect::<HashMap<_, _>>();
 
     let start_id = *dict.get("start").ok_or(Error::General("Could not find start id".into()))?;
     let end_id = *dict.get("end").ok_or(Error::General("Could not find end id".into()))?;
 
-    dbg!(&edges, start_id, end_id);
-
     let small_mask = dict.iter().filter_map(|(key, value)| {
         match key.chars().next() {
-         Some(ch) if ch.is_ascii_lowercase() => Some(*value),
+            Some(ch) if ch.is_ascii_lowercase() => Some(*value),
             _ => None
-
         }
-    }).fold(0_u64, |a, id| a | (1<<id));
+    }).fold(0_u64, |a, id| a | (1 << id));
 
-    let mut part1= 0_usize;
-        count_paths(&edges, &dict_values, small_mask, start_id, end_id, 0_u64, 0, &mut part1);
+    let mut part1 = 0_usize;
+    count_paths(&edges, small_mask, start_id, end_id, 0_u64, &mut part1);
 
     println!("Part1: {}", part1);
+
+    let mut part2 = 0_usize;
+    count_paths_part2(&edges, small_mask, start_id, start_id, end_id, 0_u64, 0_u64, &mut part2);
+
+    println!("Part2: {}", part2);
 
     Ok(())
 }
