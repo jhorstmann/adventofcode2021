db07eefaab52858d07ee6a163d78b11940bfa622
Jörn Horstmann <git@jhorstmann.net>
2021-12-15 12:41:06 +0100

    Day 15 part 2 wip

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -25,6 +25,65 @@ fn part1(map: &[Vec<u8>]) -> usize {
     last - first
 }
 
+fn part2_recursive(map: &[Vec<u8>], memo: &mut [Vec<usize>], visited: &mut [Vec<bool>], x: usize, y: usize) -> usize {
+    let value = memo[y][x];
+    if value > 0 {
+        return value as usize;
+    }
+
+    // let sum = sum + map[y][x] as usize;
+    if x == 0 && y == 0 {
+        return 0;
+    }
+
+    visited[y][x] = true;
+
+    let mut values = Vec::with_capacity(4);
+    if y > 0  {
+        values.push(part2_recursive(map, memo, visited, x, y-1));
+    }
+    if x > 0 {
+        values.push(part2_recursive(map, memo, visited, x-1, y));
+    }
+    if y < map.len()-1 && !visited[y+1][x]{
+        values.push(part2_recursive(map, memo, visited, x, y+1));
+    }
+    if x < map[y].len()-1 && !visited[y][x+1]{
+        values.push(part2_recursive(map, memo, visited, x+1, y));
+    }
+
+    let min = *values.iter().min().unwrap() + map[y][x] as usize;
+    // dbg!(x,y,min);
+    memo[y][x] = min;
+    min
+}
+
+fn part2<'a>(map: Vec<Vec<u8>>) -> usize {
+    let mut memo = map.iter().map(|row| vec![0_usize; row.len()]).collect::<Vec<_>>();
+    let mut visited = map.iter().map(|row| vec![false; row.len()]).collect::<Vec<_>>();
+
+
+    let handle = std::thread::Builder::new()
+        .stack_size(512*1024*1024)
+        .spawn(move || part2_recursive(&map, &mut memo, &mut visited, map.last().map(|row| row.len()-1).unwrap_or(0), map.len()-1)).unwrap();
+
+    let res = handle.join().unwrap();
+
+    /*
+    for y in 0..memo.len() {
+        for x in 0..memo[y].len() {
+            let value = memo[y][x];
+            eprint!("{:4}", value)
+        }
+        eprintln!();
+    }
+    eprintln!();
+*/
+
+
+    res
+}
+
 pub fn main() -> Result<()> {
     let map = include_str!("../../data/a15_input.txt")
         .lines()
@@ -32,6 +91,7 @@ pub fn main() -> Result<()> {
         .collect::<Vec<_>>();
 
     println!("Part 1: {}", part1(&map));
+    // println!("Part 1 (rec): {}", part2(map));
 
     // let map = vec![vec![9_usize]];
 
@@ -53,7 +113,7 @@ pub fn main() -> Result<()> {
             bigmap.push(bigrow);
         }
     }
-
+/*
     for y in 0..bigmap.len() {
         for x in 0..bigmap[y].len() {
             let value = bigmap[y][x];
@@ -62,10 +122,11 @@ pub fn main() -> Result<()> {
         eprintln!();
     }
     eprintln!();
-
+*/
     // 2970 to high
 
     println!("Part 2: {}", part1(&bigmap));
+    println!("Part 2 (rec): {}", part2(bigmap));
 
     Ok(())
 }
