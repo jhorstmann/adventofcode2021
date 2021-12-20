5729997112672ba13d7ff07dfa0ace1267adb49f
Jörn Horstmann <git@jhorstmann.net>
2021-12-20 12:44:15 +0100

    Cleanup

--- src/bin/a20.rs
+++ src/bin/a20.rs
@@ -1,6 +1,16 @@
 use adventofcode2021::prelude::*;
 
-const DIR: [(isize, isize); 9] = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)];
+const DIR: [(isize, isize); 9] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (0, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
 
 #[inline]
 fn get_bit(image: &[u8], width: usize, height: usize, y: usize, x: usize, dy: isize, dx: isize) -> bool {
@@ -14,9 +24,68 @@ fn get_bit(image: &[u8], width: usize, height: usize, y: usize, x: usize, dy: is
 }
 
 fn get_index(image: &[u8], width: usize, height: usize, y: usize, x: usize) -> usize {
-    DIR.iter().enumerate().fold(0_usize, |a, (i, (dx, dy))| {
-        (a<<1) | (((get_bit(image, width, height, y, x, *dy, *dx)) as usize))
+    DIR.iter().fold(0_usize, |a, (dx, dy)| {
+        (a << 1) | ((get_bit(image, width, height, y, x, *dy, *dx)) as usize)
     })
+}
+
+fn iterate(
+    original_image: &[u8],
+    original_width: usize,
+    original_height: usize,
+    lookup: &[u8],
+    iterations: usize,
+    debug: bool,
+) -> usize {
+    let border = iterations * 2 + 2;
+    let height = original_height + border * 2;
+    let width = original_width + border * 2;
+    let mut image = vec![b'.'; width * height];
+
+    for y in 0..original_width {
+        let start = (y + border) * width + border;
+        image[start..start + original_width]
+            .copy_from_slice(&original_image[y * original_width..y * original_width + original_width]);
+    }
+
+    let mut next_image = vec![0_u8; image.len()];
+
+    let mut count = 0_usize;
+
+    for i in 0..iterations {
+        count = 0;
+        for y in 0..height {
+            for x in 0..width {
+                let idx = get_index(&image, width, height, y, x);
+                next_image[y * width + x] = lookup[idx];
+            }
+        }
+
+        for y in i..height - i {
+            for x in i..width - i {
+                let b = next_image[y * width + x];
+                if b == b'#' {
+                    count += 1;
+                }
+            }
+        }
+
+        if debug {
+            for y in i..height - i {
+                for x in i..width - i {
+                    let b = next_image[y * width + x];
+                    eprint!("{}", b as char);
+                }
+                eprintln!();
+            }
+            eprintln!("{}", count);
+            eprintln!();
+        }
+
+        std::mem::swap(&mut image, &mut next_image);
+    }
+
+    count
 }
 
 pub fn main() -> Result<()> {
@@ -42,66 +111,19 @@ pub fn main() -> Result<()> {
     assert_eq!(image.len() % width, 0);
     assert_eq!(image.len() / width, width);
 
-    assert_eq!(get_index(&[b'.', b'.', b'.', b'#', b'.', b'.', b'.', b'#', b'.'], 3, 3, 1, 1), 0b000100010);
-    assert_eq!(get_index(&[b'#', b'.', b'.', b'#', b'.', b'.', b'.', b'#', b'.'], 3, 3, 1, 1), 0b100100010);
-    assert_eq!(get_index(&[b'#', b'.', b'#', b'#', b'.', b'.', b'.', b'#', b'.'], 3, 3, 1, 1), 0b101100010);
-    assert_eq!(get_index(&[b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.'], 3, 3, 1, 1), 0b000000000);
-    assert_eq!(get_index(&[b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.'], 3, 3, 0, 0), 0b000000000);
-    assert_eq!(get_index(&[b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.'], 3, 3, 2, 0), 0b000000000);
-    assert_eq!(get_index(&[b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.', b'.'], 3, 3, 2, 2), 0b000000000);
+    assert_eq!(get_index(b"...#...#.", 3, 3, 1, 1), 0b000100010);
+    assert_eq!(get_index(b"#..#...#.", 3, 3, 1, 1), 0b100100010);
+    assert_eq!(get_index(b"#.##...#.", 3, 3, 1, 1), 0b101100010);
+    assert_eq!(get_index(b".........", 3, 3, 1, 1), 0b000000000);
+    assert_eq!(get_index(b".........", 3, 3, 0, 0), 0b000000000);
+    assert_eq!(get_index(b".........", 3, 3, 2, 0), 0b000000000);
+    assert_eq!(get_index(b".........", 3, 3, 2, 2), 0b000000000);
 
-    let height = width;
+    let part1 = iterate(&image, width, width, lookup, 2, true);
+    println!("Part 1: {}", part1);
 
-    let original = image.clone();
-
-    {
-        let original_width = width;
-        let border = 102;
-        let height = height+ border*2;
-        let width = width+ border*2;
-        let mut image = vec![b'.'; (width)*(height)];
-
-        for y in 0..original_width {
-            let start = (y+border) * width + border;
-            image[start..start+original_width].copy_from_slice(&original[y*original_width..y*original_width+original_width]);
-        }
-
-
-        let mut next_image = vec![0_u8; image.len()];
-
-        for i in 0..50 {
-            for y in 0..height {
-                for x in 0..width {
-                    let mut idx = get_index(&image, width, height, y, x);
-                    let replacement = lookup[idx];
-                    next_image[y*width+x] = replacement;
-                }
-            }
-
-            // eprintln!();
-            let mut count = 0_usize;
-            for y in i..height-i {
-                for x in i..width-i {
-                    let b = next_image[y * width + x];
-                    eprint!("{}", b as char);
-                    if b == b'#' {
-                        count +=1;
-                    }
-                }
-                eprintln!();
-            }
-            eprintln!();
-
-            println!("{}", count);
-
-            std::mem::swap(&mut image, &mut next_image);
-        }
-    }
-
-    // part1: 5812 too high
-    // part2: 22617 too high
-    // part2: 20808 too high
-
+    let part2 = iterate(&image, width, width, lookup, 50, false);
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
