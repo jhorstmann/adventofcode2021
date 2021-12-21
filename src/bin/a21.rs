77858ac9645dba0acab7340c6d73e54066b59397
Jörn Horstmann <git@jhorstmann.net>
2021-12-21 15:25:34 +0100

    Day 21 part 2

--- src/bin/a21.rs
+++ src/bin/a21.rs
@@ -1,14 +1,12 @@
 use adventofcode2021::prelude::*;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 
-pub fn main() -> Result<()> {
-    let _example = [4, 8];
-    let input = [4, 10];
-
-    let mut positions = input;
+fn solve_part1(mut positions: [usize; 2]) -> usize {
     let mut scores = [0, 0];
     let mut dice = 0_usize;
     let mut rolls = 0_usize;
-    'outer: loop {
+    loop {
         for i in 0..2 {
             // die is zero-based
             let roll = dice % 100 + (dice + 1) % 100 + (dice + 2) % 100 + 3;
@@ -24,13 +22,56 @@ pub fn main() -> Result<()> {
             scores[i] += positions[i];
 
             if scores[i] >= 1000 {
-                break 'outer;
+                return scores[0].min(scores[1]) * rolls;
             }
         }
-        // dbg!(positions, scores, rolls);
     }
+}
 
-    println!("Part 1: {}", scores[0].min(scores[1]) * rolls);
+const PROBABILITIES: [usize; 7] = [1, 3, 6, 7, 6, 3, 1];
+
+fn solve_part2(positions: [usize; 2], scores: [usize; 2], player: usize) -> (usize, usize) {
+    if scores[player] >= 21 {
+        (1, 1)
+    } else if scores[1 - player] >= 21 {
+        (0, 1)
+    } else {
+        let mut new_positions = positions;
+        let mut new_scores = scores;
+
+        let mut wins = 0;
+        let mut universes = 0;
+
+        for (i, n) in PROBABILITIES.iter().enumerate() {
+            new_positions[player] = positions[player] + i + 3;
+            while new_positions[player] > 10 {
+                new_positions[player] -= 10;
+            }
+            new_scores[player] = scores[player] + new_positions[player];
+
+            if new_scores[player] < 21 {
+                let (wins1, universes1) = solve_part2(new_positions, new_scores, 1 - player);
+                wins += n * (universes1 - wins1);
+                universes += n * universes1;
+            } else {
+                wins += n;
+                universes += n;
+            }
+        }
+
+        (wins, universes)
+    }
+}
+
+pub fn main() -> Result<()> {
+    let _example = [4, 8];
+    let input = [4, 10];
+
+    println!("Part 1: {}", solve_part1(input));
+
+    let (wins, universes) = solve_part2(input, [0, 0], 0);
+
+    println!("Part 2: {:?}", wins.max(universes - wins));
 
     Ok(())
 }
